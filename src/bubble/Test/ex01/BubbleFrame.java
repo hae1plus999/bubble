16b92920a75dc5d6c015e27643ef9d09ba7a87ad
haewon999 <1000hae1@naver.com>
2023-07-05 13:36:49 +0900

    환경설정

---
+++ src/bubble/Test/ex01/BubbleFrame.java
@@ -0,0 +1,13 @@
+package bubble.Test.ex01;
+
+import javax.swing.JFrame;
+
+public class BubbleFrame extends JFrame{
+	public BubbleFrame() {
+		setSize(1000,640);
+		setVisible(true);
+	}
+	public static void main(String[] args) {
+		new BubbleFrame();
+	}
+}
---
+++ src/bubble/Test/ex02/BubbleFrame.java
@@ -0,0 +1,38 @@
+package bubble.Test.ex02;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class BubbleFrame extends JFrame{
+	
+	private JLabel backgroundMap;
+	
+	public BubbleFrame() {
+		initObject();
+		initSetting();
+		setVisible(true);
+	}
+
+	private void initObject() {
+		backgroundMap = new JLabel(new ImageIcon("image/backgroundMap.png"));
+		setContentPane(backgroundMap);
+		
+//		backgroundMap = new JLabel("Hello");
+//		backgroundMap.setSize(1000,600);
+//		add(backgroundMap);//제이프레임에 제이라벨이 그려진다
+	}
+	
+	private void initSetting() {
+		setSize(1000, 640);
+		getContentPane().setLayout(null); //레이아웃 
+		
+		setLocationRelativeTo(null); //가운데 배치
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //창끌때 같이 배치
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new BubbleFrame();
+	}
+}

f4dcb288d51e34bf68301ad1f0a9820f40b3732e
haewon999 <1000hae1@naver.com>
2023-07-10 21:43:07 +0900

    7월10일

---
+++ src/bubble/Test/ex03/BubbleFrame.java
@@ -0,0 +1,70 @@
+package bubble.Test.ex03;
+
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+//게임맵
+public class BubbleFrame extends JFrame{
+	
+	private JLabel backgroundMap;
+	private Player player;
+	
+	public BubbleFrame() {
+		initObject();
+		initSetting();
+		initListener();
+		setVisible(true);
+	}
+
+	private void initObject() {
+		backgroundMap = new JLabel(new ImageIcon("image/backgroundMap.png"));
+		setContentPane(backgroundMap);
+		
+		player = new Player();
+		add(player);
+	}
+	
+	private void initSetting() {
+		setSize(1000, 640);
+		setLayout(null); //레이아웃 
+		
+		setLocationRelativeTo(null); //가운데 배치
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //창끌때 같이 배치
+	}
+	
+	// 리스너는 os 가 키보드를 지켜보는게 리스너(while)이다.->이벤트 루프(큐)->이벤트핸들러호출(스택)
+	// 지금은 스레드가 하나밖에 없다. 오른쪽, 점프, 왼쪽, 하나만 전달이 된다.
+	private void initListener() {
+		addKeyListener(new KeyAdapter() {
+		
+			@Override
+			public void keyPressed(KeyEvent e) {
+				switch(e.getKeyCode()) {
+					case KeyEvent.VK_LEFT:
+						player.left();
+						break;
+					case KeyEvent.VK_RIGHT:
+						player.right();
+						break;
+					case KeyEvent.VK_UP:
+						player.up();
+						break;
+//					case KeyEvent.VK_DOWN:
+//						player.down();
+//						break;
+				}
+				
+			}
+		});
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new BubbleFrame();
+	}
+}

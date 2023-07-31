c44b3c71db0feec721c3a0f80d3aab89e42bd81b
haewon999 <1000hae1@naver.com>
2023-07-31 20:09:19 +0900

    1. 버블 방울 슈팅 (충돌 해결 - 스레드 무한 생성으로 프로그램 느려지는것 해결해야 함) - ex10

---
+++ src/bubble/Test/ex07/Player.java
@@ -0,0 +1,184 @@
+package bubble.Test.ex07;
+
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+
+//게임캐릭터추가
+public class Player extends JLabel implements Moveable{
+	//위치상태
+	private int x;
+	private int y;
+
+	//움직임상태
+	private boolean left;
+	private boolean right;
+	private boolean up;
+	private boolean down;
+	
+	//벽에 충돌한 상태
+	private boolean leftWallCrash;
+	private boolean rightWallCrash;
+	
+	//플레이어 속도 상태
+	private final int SPEED = 4;
+	private final int JUMPSPEED = 2; 
+	
+	private ImageIcon playerR, playerL;
+
+	private void initObject() {
+		playerR = new ImageIcon("image/playerR.png");
+		playerL = new ImageIcon("image/playerL.png");
+	}
+	
+	private void initSetting() {
+		x = 80;
+		y = 535;
+		
+		left = false;
+		right = false;
+		up = false;
+		down = false;
+		
+		leftWallCrash = false;
+		rightWallCrash = false;
+		
+		setIcon(playerR);
+		setSize(50,50);
+		setLocation(x,y);
+	}
+
+	private void initBackgroundPlayerService() {
+		new Thread(new BackgroundPlayerService(this)).start();
+	}
+	
+	//이벤트핸들러
+	@Override
+	public void left() {
+		left =true;
+		new Thread(()-> {
+			while(left) {
+				x = x - SPEED;
+				setLocation(x,y);
+				setIcon(playerL);
+				try {
+					//0.01초
+					Thread.sleep(10);
+				}catch(InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		}).start();
+	}
+
+	@Override
+	public void right() {
+		
+		right = true;
+		new Thread(()-> {
+			while(right) {
+				x = x + SPEED;
+				setLocation(x,y);
+				setIcon(playerR);
+				try {
+					//0.01초
+					Thread.sleep(10);
+				}catch(InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		}).start();
+	}
+
+	//left + up, right + up
+	@Override
+	public void up() {
+		up = true;
+		new Thread(()->{
+			for(int i=0; i<130/JUMPSPEED; i++) {
+				y = y - JUMPSPEED;
+				setLocation(x,y);
+		
+				try {
+					Thread.sleep(5);
+				}catch(Exception e){
+					e.printStackTrace();
+				}	
+			}
+			up = false;
+			down();
+			
+		}).start();
+	}
+
+	@Override
+	public void down() {
+		down = true;
+		new Thread(()->{
+			for(int i=0; i<130/JUMPSPEED; i++) {
+				y = y + JUMPSPEED;
+				setLocation(x,y);
+		
+				try {
+					Thread.sleep(3);
+				}catch(Exception e){
+					e.printStackTrace();
+				}	
+			}
+			down = false;
+		}).start();
+	}
+	
+	public Player() {
+		initObject();
+		initSetting();
+		initBackgroundPlayerService();
+	}
+	
+	public boolean isLeft() {
+		return left;
+	}
+
+	public void setLeft(boolean left) {
+		this.left = left;
+	}
+
+	public boolean isRight() {
+		return right;
+	}
+
+	public void setRight(boolean right) {
+		this.right = right;
+	}
+
+	public boolean isUp() {
+		return up;
+	}
+
+	public void setUp(boolean up) {
+		this.up = up;
+	}
+
+	public boolean isDown() {
+		return down;
+	}
+
+	public void setDown(boolean down) {
+		this.down = down;
+	}
+	
+	public boolean isLeftWallCrash() {
+		return leftWallCrash;
+	}
+
+	public void setLeftWallCrash(boolean leftWallCrash) {
+		this.leftWallCrash = leftWallCrash;
+	}
+
+	public boolean isRightWallCrash() {
+		return rightWallCrash;
+	}
+
+	public void setRightWallCrash(boolean rightWallCrash) {
+		this.rightWallCrash = rightWallCrash;
+	}
+}

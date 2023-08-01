45fe0891521e5b9bb15a628fa26de6fadfcd9ef4
haewon999 <1000hae1@naver.com>
2023-08-01 11:33:19 +0900

    1.  버블 방울 슈팅 - 버블 자체 스레드로 해결 - 끝 - ex11

---
+++ src/bubble/Test/ex11/Moveable.java
@@ -0,0 +1,11 @@
+package bubble.Test.ex11;
+
+public interface Moveable {
+	public abstract void left();
+	public abstract void right();
+	public abstract void up();
+	//default를 사용하면 인터페이스도 몸체가 있는 메서드응 만들 수 있다. 다중상속이 안되는것이 많기 때문에
+	//어댑터 패턴보다는 디폴트를 사용하는 것이 좋다
+	default public void down() {};
+
+}
---
+++ src/bubble/Test/ex11/PlayerWay.java
@@ -0,0 +1,5 @@
+package bubble.Test.ex11;
+
+public enum PlayerWay {
+	LEFT,RIGHT;
+}
